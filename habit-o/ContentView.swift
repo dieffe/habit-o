881837e5844322fa02f06749d5e737fb73d1c466
Fausto <dieffe@gmail.com>
2022-08-11 18:22:29 +0200

    Initial Commit

---
+++ habit-o/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  habit-o
+//
+//  Created by fausto.dassenno on 11/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ habit-o/habit_oApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_oApp.swift
+//  habit-o
+//
+//  Created by fausto.dassenno on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct habit_oApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
